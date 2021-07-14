a1d0a2318bd3867e844541c337602716c424e790
Murilo Baptista <muboeira@gmail.com>
2021-07-13 22:08:53 -0300

    better solution to filtering odd numbers out of string list

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -2,18 +2,12 @@ defmodule ListFilter do
   require Integer
 
   def call(list) do
-    Enum.reduce(list, 0, fn item, acc ->
-      case Integer.parse(item) do
-        {int, _decimal} ->
-          if Integer.is_odd(int) do
-            acc + 1
-          else
-            acc
-          end
-
-        _ ->
-          acc
+    Enum.flat_map(list, fn string ->
+      case Integer.parse(string) do
+        {int, _rest} -> [int]
+        :error -> []
       end
     end)
+    |> Enum.count(&Integer.is_odd(&1))
   end
 end
